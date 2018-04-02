46cce2eeb69605bb35f432498edd38d836f0bacf
Sam Sieber <swsieber@gmail.com>
2018-04-02 13:58:20 -0600

    Disable sandbox

--- 2. search/amazon.js
+++ 2. search/amazon.js
@@ -7,7 +7,7 @@ const puppeteer = require('puppeteer')
 const screenshot = 'amazon_nyan_cat_pullover.png'
 try {
   (async () => {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({args: ['--no-sandbox','--disable-setuid-sandbox']})
     const page = await browser.newPage()
     await page.setViewport({ width: 1280, height: 800 })
     await page.goto('https://www.amazon.com')
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-const browser = await puppeteer.launch({ headless: false })
+const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
 const page = await browser.newPage()
 
 await page.setViewport({ width: 1280, height: 800 })
